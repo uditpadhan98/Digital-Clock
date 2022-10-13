265756887e415c5048075f640ad10b38d98d44fa
uditpadhan98 <105899769+uditpadhan98@users.noreply.github.com>
2022-10-13 11:22:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function updateTime(){
+    var dateTime=new Date();
+    var day=dateTime.getDay();
+    var hours=dateTime.getHours();
+    var minutes=dateTime.getMinutes();
+    var seconds=dateTime.getSeconds();
+    var am_pm=document.getElementById("am_pm");
+    var dayNames=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    if (hours>=12) {
+        am_pm.innerHTML="PM";
+    }
+    else{
+        am_pm.innerHTML="AM";
+    }
+
+    if (hours>12) {
+        hours=hours-12;
+    }
+    document.getElementById("day").innerHTML=dayNames[day];
+    document.getElementById("hours").innerHTML=hours;
+    document.getElementById("minutes").innerHTML=minutes;
+    document.getElementById("seconds").innerHTML=seconds;
+}
+setInterval(updateTime,1000);
+var dateTime=new Date();
+var month=dateTime.getMonth();
+var monthNames=["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"]
+document.getElementById("month").innerHTML=monthNames[month+1];
+var date=dateTime.getDate();
+document.getElementById("date").innerHTML=date;
+var year=dateTime.getFullYear();
+document.getElementById("year").innerHTML=year;
